869dca0b05bcb95f5cd55fcb9e8a7bcb946c38ed
Meghashyam <shyamms17@hotmail.com>
2024-06-25 12:25:43 +0530

    Updated commit

--- build/lib/pyls.py
+++ build/lib/pyls.py
@@ -1,418 +1,141 @@
-# import json
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def print_ls(directory):
-#     if 'contents' in directory:
-#         for item in directory['contents']:
-#             if not item['name'].startswith('.'):
-#                 print(item['name'])
-
-# def main():
-#     json_file = 'structure.json'
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure)
-
-# if __name__ == "__main__":
-#     main()
-
-# import json
-# import sys
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def print_ls(directory, show_all=False):
-#     if 'contents' in directory:
-#         for item in directory['contents']:
-#             if show_all  or not item['name'].startswith('.'):
-#                 print(item['name'])
-            
-# def main():
-#     json_file = 'structure.json'
-    
-#     show_all = '-A' in sys.argv
-
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure, show_all)
-
-# if __name__ == "__main__":
-#     main()
-
-# import json
-# import sys
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def print_ls(directory, show_all=False, vertical=False):
-#     if 'contents' in directory:
-#         for item in directory['contents']:
-#             if show_all or not item['name'].startswith('.'):
-#                 if vertical:
-#                     #print(item[])
-#                     print(f"{item['permissions']}, {item['size']}, {item['time_modified']},{item['name']} ")
-#                 else:
-#                     print(item['name'])
-
-# def main():
-#     json_file = 'structure.json'
-    
-#     show_all = '-A' in sys.argv
-#     vertical = '-l' in sys.argv
-
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure, show_all, vertical)
-
-# if __name__ == "__main__":
-#     main()
-
-# import json
-# import sys
-# from datetime import datetime
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def format_time(timestamp):
-#     return datetime.fromtimestamp(timestamp).strftime('%b %d %H:%M')
-
-# def print_ls(directory, show_all=False, vertical=False):
-#     if 'contents' in directory:
-#         for item in directory['contents']:
-#             if show_all or not item['name'].startswith('.'):
-#                 if vertical:
-#                     formatted_time = format_time(item['time_modified'])
-#                     print(f"{item['permissions']} {item['size']} {formatted_time} {item['name']}")
-#                 else:
-#                     print(item['name'])
-
-# def main():
-#     json_file = 'structure.json'
-    
-#     show_all = '-A' in sys.argv
-#     vertical = '-l' in sys.argv
-
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure, show_all, vertical)
-
-# if __name__ == "__main__":
-#     main()
-
-# import json
-# import sys
-# from datetime import datetime
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def format_time(timestamp):
-#     return datetime.fromtimestamp(timestamp).strftime('%b %d %H:%M')
-
-# def print_ls(directory, show_all=False, vertical=False, reverse=False):
-#     if 'contents' in directory:
-#         contents = directory['contents']
-#         if reverse:
-#             contents = contents[::-1]
-#         for item in contents:
-#             if show_all or not item['name'].startswith('.'):
-#                 if vertical:
-#                     formatted_time = format_time(item['time_modified'])
-#                     print(f"{item['permissions']} {item['size']} {formatted_time} {item['name']}")
-#                 else:
-#                     print(item['name'])
-
-# def main():
-#     json_file = 'structure.json'
-    
-#     show_all = '-A' in sys.argv
-#     vertical = '-l' in sys.argv
-#     reverse = '-r' in sys.argv
-
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure, show_all, vertical, reverse)
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
-# import json
-# import sys
-# from datetime import datetime
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def format_time(timestamp):
-#     return datetime.fromtimestamp(timestamp).strftime('%b %d %H:%M')
-
-# def print_ls(directory, show_all=False, vertical=False, reverse=False):
-#     if 'contents' in directory:
-#         contents = directory['contents']
-#         if reverse:
-#             contents = reversed(contents)
-#         for item in contents:
-#             if show_all or not item['name'].startswith('.'):
-#                 if vertical:
-#                     formatted_time = format_time(item['time_modified'])
-#                     print(f"{item['permissions']} {item['size']} {formatted_time} {item['name']}")
-#                 elif reverse:
-#                     formatted_time = format_time(item['time_modified'])
-#                     print(f"{item['permissions']} {item['size']} {formatted_time} {item['name']}")
-#                 else:
-#                     print(item['name'])
-
-# def main():
-#     json_file = 'structure.json'
-    
-#     show_all = '-A' in sys.argv
-#     vertical = '-l' in sys.argv
-#     reverse = '-r' in sys.argv
-
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure, show_all, vertical, reverse)
-
-# if __name__ == "__main__":
-#     main()
-
-# import json
-# import sys
-# from datetime import datetime
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def format_time(timestamp):
-#     return datetime.fromtimestamp(timestamp).strftime('%b %d %H:%M')
-
-# def print_ls(directory, show_all=False, vertical=False, reverse=False, sort_by_time=False):
-#     if 'contents' in directory:
-#         contents = directory['contents']
-#         if sort_by_time:
-#             contents = sorted(contents, key=lambda x: x['time_modified'])
-#         if reverse:
-#             contents = list(reversed(contents))
-#         for item in contents:
-#             if show_all or not item['name'].startswith('.'):
-#                 if vertical:
-#                     formatted_time = format_time(item['time_modified'])
-#                     print(f"{item['permissions']} {item['size']} {formatted_time} {item['name']}")
-#                 else:
-#                     print(item['name'])
-
-# def main():
-#     json_file = 'structure.json'
-    
-#     show_all = '-A' in sys.argv
-#     vertical = '-l' in sys.argv
-#     reverse = '-r' in sys.argv
-#     sort_by_time = '-t' in sys.argv
-
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure, show_all, vertical, reverse, sort_by_time)
-
-# if __name__ == "__main__":
-#     main()
-
-# import json
-# import sys
-# from datetime import datetime
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def format_time(timestamp):
-#     return datetime.fromtimestamp(timestamp).strftime('%b %d %H:%M')
-
-# def print_ls(directory, show_all=False, vertical=False, reverse=False, sort_by_time=False, filter_option=None):
-#     if 'contents' in directory:
-#         contents = directory['contents']
-        
-#         if filter_option == 'file':
-#             contents = [item for item in contents if not item['permissions'].startswith('d')]
-#         elif filter_option == 'dir':
-#             contents = [item for item in contents if item['permissions'].startswith('d')]
-#         elif filter_option:
-#             print(f"error: '{filter_option}' is not a valid filter criteria. Available filters are 'dir' and 'file'")
-#             return
-
-#         if sort_by_time:
-#             contents = sorted(contents, key=lambda x: x['time_modified'])
-#         if reverse:
-#             contents = list(reversed(contents))
-            
-#         for item in contents:
-#             if show_all or not item['name'].startswith('.'):
-#                 if vertical:
-#                     formatted_time = format_time(item['time_modified'])
-#                     print(f"{item['permissions']} {item['size']} {formatted_time} {item['name']}")
-#                 else:
-#                     print(item['name'])
-
-# def main():
-#     json_file = 'structure.json'
-    
-#     show_all = '-A' in sys.argv
-#     vertical = '-l' in sys.argv
-#     reverse = '-r' in sys.argv
-#     sort_by_time = '-t' in sys.argv
-
-#     filter_option = None
-#     for arg in sys.argv:
-#         if arg.startswith('--filter='):
-#             filter_option = arg.split('=')[1]
-#             break
-
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure, show_all, vertical, reverse, sort_by_time, filter_option)
-
-# if __name__ == "__main__":
-#     main()
-
-
-# import json
-# import sys
-# from datetime import datetime
-
-# def load_directory_structure(json_file):
-#     with open(json_file, 'r') as f:
-#         return json.load(f)
-
-# def format_time(timestamp):
-#     return datetime.fromtimestamp(timestamp).strftime('%b %d %H:%M')
-
-# def print_ls(directory, show_all=False, vertical=False, reverse=False, sort_by_time=False, filter_option=None):
-#     if 'contents' in directory:
-#         contents = directory['contents']
-        
-#         if filter_option == 'file':
-#             contents = [item for item in contents if not item['permissions'].startswith('d')]
-#         elif filter_option == 'dir':
-#             contents = [item for item in contents if item['permissions'].startswith('d')]
-#         elif filter_option:
-#             print(f"error: '{filter_option}' is not a valid filter criteria. Available filters are 'dir' and 'file'")
-#             return
-
-#         if sort_by_time:
-#             contents = sorted(contents, key=lambda x: x['time_modified'])
-#         if reverse:
-#             contents = list(reversed(contents))
-            
-#         for item in contents:
-#             if show_all or not item['name'].startswith('.'):
-#                 if vertical:
-#                     formatted_time = format_time(item['time_modified'])
-#                     print(f"{item['permissions']} {item['size']} {formatted_time} {item['name']}")
-#                 else:
-#                     print(item['name'])
-
-# def print_help():
-#     print("Usage: python script.py [OPTIONS]")
-#     print("Options:")
-#     print("  -A       show all files")
-#     print("  -l       use a long listing format")
-#     print("  -r       reverse order")
-#     print("  -t       sort by time modified")
-#     print("  --filter=TYPE   filter by 'dir' or 'file'")
-#     print("  --help   display this help and exit")
-
-# def main():
-#     json_file = 'structure.json'
-    
-#     if '--help' in sys.argv:
-#         print_help()
-#         sys.exit(0)
-    
-#     show_all = '-A' in sys.argv
-#     vertical = '-l' in sys.argv
-#     reverse = '-r' in sys.argv
-#     sort_by_time = '-t' in sys.argv
-
-#     filter_option = None
-#     for arg in sys.argv:
-#         if arg.startswith('--filter='):
-#             filter_option = arg.split('=')[1]
-#             break
-
-#     directory_structure = load_directory_structure(json_file)
-#     print_ls(directory_structure, show_all, vertical, reverse, sort_by_time, filter_option)
-
-# if __name__ == "__main__":
-#     main()
 
 import json
 import sys
+import os
 from datetime import datetime
 
-def load_directory_structure(json_file):
+# Global variables
+json_file = 'structure.json'
+current_directory = os.getcwd()
+
+# Load directory structure from JSON file
+def load_directory_structure():
     with open(json_file, 'r') as f:
         return json.load(f)
 
+# Format timestamp to specified format
 def format_time(timestamp):
     return datetime.fromtimestamp(timestamp).strftime('%b %d %H:%M')
 
+# Convert size to human readable format
 def human_readable_size(size):
-    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
-        if size < 1024.0:
-            return f"{size:.1f}{unit}"
-        size /= 1024.0
-    return f"{size:.1f}PB"
+    units = ['B', 'KB', 'MB', 'GB', 'TB']
+    index = 0
+    while size >= 1024 and index < len(units) - 1:
+        size /= 1024
+        index += 1
+    return f"{size:.1f}{units[index]}"
 
+# Print file details
+def print_file_details(file, human_readable):
+    formatted_time = format_time(file['time_modified'])
+    size = human_readable_size(file['size']) if human_readable else file['size']
+    print(f"{file['permissions']} {size} {formatted_time} {file['name']}")
+
+# Print the directory listing
 def print_ls(directory, show_all=False, vertical=False, reverse=False, sort_by_time=False, filter_option=None, human_readable=False):
-    if 'contents' in directory:
-        contents = directory['contents']
-        
+    if 'contents' not in directory:
+        print_file_details(directory, human_readable)
+        return
+    
+    contents = directory['contents']
+    
+    # Filter hidden files
+    if not show_all:
+        contents = [item for item in contents if not item['name'].startswith('.')]
+
+    # Apply file/directory filter if specified
+    if filter_option:
         if filter_option == 'file':
             contents = [item for item in contents if not item['permissions'].startswith('d')]
         elif filter_option == 'dir':
             contents = [item for item in contents if item['permissions'].startswith('d')]
-        elif filter_option:
+        else:
             print(f"error: '{filter_option}' is not a valid filter criteria. Available filters are 'dir' and 'file'")
             return
 
-        if sort_by_time:
-            contents = sorted(contents, key=lambda x: x['time_modified'])
-        if reverse:
-            contents = list(reversed(contents))
-            
+    # Sort by time if specified
+    if sort_by_time:
+        contents = sorted(contents, key=lambda x: x['time_modified'])
+
+    # Reverse order if specified
+    if reverse:
+        contents.reverse()
+
+    # Print in long format if specified
+    if vertical:
         for item in contents:
-            if show_all or not item['name'].startswith('.'):
-                if vertical:
-                    formatted_time = format_time(item['time_modified'])
-                    size = human_readable_size(item['size']) if human_readable else item['size']
-                    print(f"{item['permissions']} {size} {formatted_time} {item['name']}")
-                else:
-                    print(item['name'])
+            print_file_details(item, human_readable)
+    else:
+        for item in contents:
+            print(item['name'])
 
+# Show help message
 def print_help():
-    print("Usage: python script.py [OPTIONS]")
+    print("Usage: python -m pyls [OPTIONS] [PATH]")
     print("Options:")
-    print("  -A           show all files")
-    print("  -l           use a long listing format")
-    print("  -r           reverse order")
-    print("  -t           sort by time modified")
-    print("  -h           display sizes in human readable format")
-    print("  --filter=TYPE filter by 'dir' or 'file'")
-    print("  --help       display this help and exit")
+    print("  -A               show all files")
+    print("  -l               use a long listing format")
+    print("  -r               reverse order")
+    print("  -t               sort by time modified")
+    print("  -h               display sizes in human readable format")
+    print("  --filter=TYPE    filter by 'dir' or 'file'")
+    print("  --help           display this help and exit")
 
+# Main function
 def main():
-    json_file = 'structure.json'
-    
-    if '--help' in sys.argv:
-        print_help()
-        sys.exit(0)
-    
+    global json_file
+
+    # Check if a specific path is provided
+    if len(sys.argv) > 1 and not sys.argv[1].startswith('-'):
+        path = sys.argv[1]
+        
+        # Construct full path
+        json_file = os.path.join(current_directory, path)
+        
+        # Check if the path exists
+        if not os.path.exists(json_file):
+            print(f"error: cannot access '{path}': No such file or directory")
+            sys.exit(1)
+
+    directory = load_directory_structure()
+
+    # Handle the case where a path is specified after options
+    if len(sys.argv) > 2 and not sys.argv[2].startswith('-'):
+        path = sys.argv[2]
+
+        # Split the path to handle relative paths correctly
+        if path.startswith('./'):
+            path = path[2:]
+        elif path == '.':
+            path = ''
+        
+        # Traverse the directory structure to find the requested path
+        path_parts = path.split('/')
+        for part in path_parts[:-1]:  # Traverse until the parent directory
+            found = False
+            for item in directory['contents']:
+                if item['name'] == part and item['permissions'].startswith('d'):
+                    directory = item
+                    found = True
+                    break
+            if not found:
+                print(f"error: cannot access '{sys.argv[2]}': No such file or directory")
+                sys.exit(1)
+        
+        # Check if the last part is a file or directory
+        target_name = path_parts[-1]
+        found = False
+        for item in directory['contents']:
+            if item['name'] == target_name:
+                directory = item
+                found = True
+                break
+        if not found:
+            print(f"error: cannot access '{sys.argv[2]}': No such file or directory")
+            sys.exit(1)
+
+    # Process options
     show_all = '-A' in sys.argv
     vertical = '-l' in sys.argv
     reverse = '-r' in sys.argv
@@ -425,8 +148,10 @@ def main():
             filter_option = arg.split('=')[1]
             break
 
-    directory_structure = load_directory_structure(json_file)
-    print_ls(directory_structure, show_all, vertical, reverse, sort_by_time, filter_option, human_readable)
+    print_ls(directory, show_all, vertical, reverse, sort_by_time, filter_option, human_readable)
 
 if __name__ == "__main__":
-    main()
+    if '--help' in sys.argv:
+        print_help()
+    else:
+        main()
